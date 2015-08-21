29ac5ab4ae3fecd5ce6e2067d32a7cb2130a64a0
Ben Jones <ben@bnjns.uk>
2015-08-21 21:10:59 +0200

    Add FAQ to footer

--- resources/views/partials/app/footer/right.blade.php
+++ resources/views/partials/app/footer/right.blade.php
@@ -3,5 +3,6 @@
     <li><a href="{{ route('contact.enquiries') }}">Contact Us</a></li>
     <li><a href="{{ route('contact.book') }}">Book Us</a></li>
     <li><a href="{{ route('contact.book.terms') }}">Terms and Conditions</a></li>
+    <li><a href="{{ route('page.show', 'faq') }}">Frequently Asked Questions</a></li>
     <li><a href="{{ route('page.show', 'links') }}">Useful Links</a></li>
 </ul>
