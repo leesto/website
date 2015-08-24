bf8046f32e792560abba136a97824ed445d73504
Ben Jones <ben@bnjns.uk>
2015-08-25 00:13:14 +0200

    Update event paperwork

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -95,10 +95,26 @@
                             </div>
                         </div>
                     @endif
+                    {{-- Paperwork --}}
                     @if($canEdit)
                         <h2>Paperwork</h2>
                         @foreach(\App\Event::$Paperwork as $key => $name)
-                            @include('events._paperwork_status', ['key' => $key, 'name' => $name])
+                            <div class="form-group">
+                                {!! Form::label('', $name . ':', ['class' => 'col-md-5 control-label']) !!}
+                                <div class="col-md-7">
+                                    <p class="form-control-static">
+                                        <span class="paperwork" data-key="{{ $key }}" data-status="{{ $event->paperwork[$key] }}">
+                                            @if($event->paperwork[$key])
+                                                <span class="fa fa-check"></span>
+                                                <span>completed</span>
+                                            @else
+                                                <span class="fa fa-remove"></span>
+                                                <span>not completed</span>
+                                            @endif
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
                         @endforeach
                     @endif
                 </div>
