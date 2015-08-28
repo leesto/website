e66a45f1b6e4dbbce8de6dd05ae27da0eac16557
Ben Jones <ben@bnjns.uk>
2015-08-28 14:32:01 +0100

    Prepend all email subjects with common string

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,30 +4,29 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array
-     */
-    protected $listen = [
-        'App\Events\SomeEvent' => [
-            'App\Listeners\EventListener',
-        ],
-    ];
+	/**
+	 * The event listener mappings for the application.
+	 * @var array
+	 */
+	protected $listen = [];
 
-    /**
-     * Register any other events for your application.
-     *
-     * @param  \Illuminate\Contracts\Events\Dispatcher  $events
-     * @return void
-     */
-    public function boot(DispatcherContract $events)
-    {
-        parent::boot($events);
+	/**
+	 * Register any other events for your application.
+	 * @param  \Illuminate\Contracts\Events\Dispatcher $events
+	 * @return void
+	 */
+	public function boot(DispatcherContract $events)
+	{
+		parent::boot($events);
 
-        //
-    }
+		// Listen to all emails to prepend the
+		// subject with '[Backstage Website]'
+		Event::listen('mailer.sending', function ($message) {
+			$message->setSubject('[Backstage Website] ' . $message->getSubject());
+		});
+	}
 }
