48a99eb82b623422abbf2b4237201585b479eeb7
Ben Jones <ben@bnjns.uk>
2015-09-09 17:31:31 +0100

    Fix styling of error messages for mobiles
    This also disables the incomplete events diary gantt chart functionality.

--- resources/assets/js/partials/diary.js
+++ resources/assets/js/partials/diary.js
@@ -5,16 +5,16 @@ $modal.on('click', '#submitDateModal', function () {
 		.replace('%year', form.find('select[name="year"]').val())
 		.replace('%month', form.find('select[name="month"]').val());
 });
-$modal.on('show.bs.modal', function(event) {
-	var btn = $(event.relatedTarget);
-	if(btn.data('modalTemplate') == 'date_gantt') {
-		var header = $modal.find('div.modal-header').children('h1');
-		header.text(btn.data('date'));
-
-
-	}
-	console.log(btn);
-});
+//$modal.on('show.bs.modal', function(event) {
+//	var btn = $(event.relatedTarget);
+//	if(btn.data('modalTemplate') == 'date_gantt') {
+//		var header = $modal.find('div.modal-header').children('h1');
+//		header.text(btn.data('date'));
+//
+//
+//	}
+//	console.log(btn);
+//});
 $('#diary').on('click', '.cell a', function(event) {
 	event.stopPropagation();
 });
